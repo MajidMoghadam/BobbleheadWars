f9b5a83dc53343445ed3d5678d419bca08c0393d
Majid Moghadam <majid.moghadam114@yahoo.com>
2019-09-14 15:38:08 -0400

    move camera so it follows player

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    
+    public GameObject followTarget;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followTarget != null)
+        {
+            //this makes the camera mount position smoothly move to where the player is over time
+            transform.position = Vector3.Lerp(transform.position, 
+                followTarget.transform.position, Time.deltaTime * moveSpeed);
+        }
+    }
+}
