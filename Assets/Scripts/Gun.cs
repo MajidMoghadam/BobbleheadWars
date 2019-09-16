af0e3ccef897944bb84c2bea8cc37779305e81e0
Majid Moghadam <majid.moghadam114@yahoo.com>
2019-09-16 14:12:30 -0400

    created bullet asset and ability of marine to fire bullets

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform launchPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!IsInvoking("fireBullet"))
+            {
+                InvokeRepeating("fireBullet", 0f, 0.1f);
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            CancelInvoke("fireBullet");
+        }
+    }
+
+    void fireBullet()
+    {
+        // Instantiate creates an instance of the bulletPrefab, of type Object, so we must cast is as GameObject
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        // the bullets position is set to the launchers position
+        bullet.transform.position = launchPosition.position;
+        // bullet will be attached to marine (parent) so it will travel in same forward direction as marine faces
+        bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
+    }
+}
