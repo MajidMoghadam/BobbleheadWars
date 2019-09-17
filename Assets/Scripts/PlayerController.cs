f50c0856da7af0805d3558af4de2387fd70079b3
Majid Moghadam <majid.moghadam114@yahoo.com>
2019-09-16 22:02:40 -0400

    added player controller and collumn colliders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,26 +5,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
+    //reference to character controller of space marine
+    private CharacterController characterController;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //get the onle and only character controller component in the game
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //get the position of the current game object (the one the script is attached to)
-        Vector3 pos = transform.position;
-
-        //calculate the distance travelled left and right in one frame (d = v * t) 
-        //the GetAxis input is either +1, 0 or -1 (gives it direction)
-        //the += means you add the small distance moved in one frame along to the original value of x
-        pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
-        //similar to x-axis but along z-axis, up and down
-        pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-        //alter the position of the game object to what the same as pos
-        transform.position = pos;
+        //store the movement direction in the x, z plane
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
+        //Simple move is a built in function of character controller, move in specified direction
+        characterController.SimpleMove(moveDirection * moveSpeed);
     }
 }
