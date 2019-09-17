f904a2297b92cb5f32b6590fd38f58e090df8d1c
Majid Moghadam <majid.moghadam114@yahoo.com>
2019-09-17 11:45:09 -0400

    set up GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //used to keep track of the players position
+    public GameObject player;
+    //keep track of all spawn points
+    public GameObject[] spawnPoints;
+    //used to spawn new aliens
+    public GameObject alien;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
