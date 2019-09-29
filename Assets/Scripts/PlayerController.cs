c4b00f4a89f127a2b766eb611b3cf05c5dd7e599
Majid Moghadam <majid.moghadam114@yahoo.com>
2019-09-29 11:51:13 -0400

    added player death & head decapitation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,6 +21,9 @@ public class PlayerController : MonoBehaviour
     private float timeSinceHit = 0;
     private int hitNumber = -1;
 
+    public Rigidbody marineBody;
+    private bool isDead = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -100,7 +103,7 @@ public class PlayerController : MonoBehaviour
                 }
                 else
                 {
-                    // death todo
+                    Die();
                 }
                 isHit = true; // 4
                 SoundManager.Instance.PlayOneShot(SoundManager.Instance.hurt);
@@ -108,5 +111,21 @@ public class PlayerController : MonoBehaviour
             alien.Die();
         }
     }
-   
+
+    public void Die()
+    {
+        bodyAnimator.SetBool("IsMoving", false);
+        marineBody.transform.parent = null;
+        marineBody.isKinematic = false;
+        marineBody.useGravity = true;
+        marineBody.gameObject.GetComponent<CapsuleCollider>().enabled = true;
+        marineBody.gameObject.GetComponent<Gun>().enabled = false;
+
+        Destroy(head.gameObject.GetComponent<HingeJoint>());
+        head.transform.parent = null;
+        head.useGravity = true;
+        SoundManager.Instance.PlayOneShot(SoundManager.Instance.marineDeath);
+        Destroy(gameObject);
+    }
+
 }
